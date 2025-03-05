8d4a09fab40cef31255ef492ec3a3477d8bc3f79
Andrew-8705 <abaldin05@mail.ru>
2025-03-05 14:22:14 +0300

    expression

--- LAB1_POLYNOM/polynomial_algebra/expression.h
+++ LAB1_POLYNOM/polynomial_algebra/expression.h
@@ -1,1 +1,37 @@
 #pragma once
+
+#include "../polynom/polynom.h"
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Parser { 
+public:
+	static vector<string> Parse(const string& str);
+};
+
+class InfixToPostfixConverter { 
+public:
+	static map<string, int> priority;
+
+	static string Convert(const string& infix);
+};
+
+class PostfixCalculator { 
+public:
+    static Polynom Calculate(const string& postfix);
+};
+
+class Expression {
+    string infix;
+    string postfix;
+    map<string, Polynom> operands; // возможно пригодится для хранения полиномов которые присутствуют в выражении
+public:
+    Expression(const string& expr = "");
+    string GetInfix() const;
+    string GetPostfix() const;
+    vector<string> GetOperands() const;
+    Polynom Calculate();
+};
