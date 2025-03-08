760bf4338cc969ffb233756bfafeb74a972baa5c
Andrew-8705 <abaldin05@mail.ru>
2025-03-08 21:17:08 +0300

    test_expression

--- LAB1_POLYNOM/base_test/test_expression.cpp
+++ LAB1_POLYNOM/base_test/test_expression.cpp
@@ -1,2 +1,63 @@
 #include "gtest.h"
 #include "../polynomial_algebra/expression.h"
+
+TEST(Parser, can_parse_simple_expression)
+{
+	ADD_FAILURE();
+}
+
+TEST(Parser, can_parse_expression_with_parentheses)
+{
+	ADD_FAILURE();
+}
+
+TEST(InfixToPostfixConverter, can_convert_simple_expression)
+{
+	ADD_FAILURE();
+}
+
+TEST(InfixToPostfixConverter, can_convert_expression_with_parentheses) 
+{
+	ADD_FAILURE();
+}
+
+TEST(PostfixCalculator, can_calculate_simple_expression)
+{
+	ADD_FAILURE();
+}
+
+TEST(PostfixCalculator, can_calculate_expression_with_multiplication)
+{
+	ADD_FAILURE();
+}
+
+TEST(PostfixCalculator, can_calculate_expression_with_subtraction)
+{
+	ADD_FAILURE();
+}
+
+TEST(Expression, can_create_with_valid_expression)
+{
+	ADD_FAILURE();
+}
+
+TEST(Expression, can_get_infix)
+{
+	ADD_FAILURE();
+}
+
+TEST(Expression, can_get_postfix)
+{
+	ADD_FAILURE();
+}
+
+TEST(Expression, can_calculate_expression)
+{
+	ADD_FAILURE();
+}
+
+TEST(Expression, can_get_operands_from_expression)
+{
+	ADD_FAILURE();
+}
+
