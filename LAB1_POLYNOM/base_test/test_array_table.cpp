5d78ee504200cfe637f4a2da5d3d8104963994d4
fartekool <levonychev2006@gmail.com>
2025-03-20 22:56:31 +0300

    some tests for array_table

--- LAB1_POLYNOM/base_test/test_array_table.cpp
+++ LAB1_POLYNOM/base_test/test_array_table.cpp
@@ -8,17 +8,34 @@ TEST(Array_table, can_create_table)
 
 TEST(Array_table, can_insert_elem)
 {
-	ADD_FAILURE();
+	Array_table<string, int> a;
+	ASSERT_NO_THROW(a.insert("name", 4));
+	EXPECT_TRUE(a.insert("name2", 10));
 }
 
-TEST(Array_table, throws_when_insert_if_table_is_full)
+TEST(Array_table, expected_false_when_insert_if_table_is_full)
 {
-	ADD_FAILURE();
+	Array_table<int, int> a;
+	for (int i = 0; i < max_fill; ++i)
+		a.insert(i, 4);
+	EXPECT_FALSE(a.insert(1000, 4));
+}
+
+TEST(Array_table, expected_false_when_insert_existing_element)
+{
+	Array_table<string, int> a;
+	a.insert("name", 4);
+	EXPECT_FALSE(a.insert("name", 5));
 }
 
 TEST(Array_table, can_delete_elem)
-{
-	ADD_FAILURE();
+{	
+	Array_table<string, int> a;
+	a.insert("a", 1);
+	ASSERT_NO_THROW(a.delete_rec("a"));
+	a.insert("b", 1);
+	EXPECT_TRUE(a.delete_rec("b"));
+
 }
 
 TEST(Array_table, throws_when_delete_if_table_is_empty)
