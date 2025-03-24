3b5601bb92b3a4e86f295fcef7ea63a1f0f222df
Иван Лукин <dizbvalkyre@gmail.com>
2025-03-24 20:49:47 +0300

    table manager

---
+++ LAB1_POLYNOM/base_test/test_table_manager.cpp
@@ -0,0 +1,96 @@
+#include "gtest.h"
+#include "../tables/tables_manager.h"
+
+TEST(Tables_manager, can_create_manager)
+{
+	ASSERT_NO_THROW(decltype(Tables_manager<string, int> ())a);
+}
+
+TEST(Tables_manager, can_insert_element_in_tables)
+{
+	Tables_manager<string, int> a;
+	EXPECT_TRUE(a.insert("name1", 1));
+}
+
+TEST(Tables_manager, false_when_insert_existing_element_in_tables)
+{
+	Tables_manager<string, int> a;
+	a.insert("name1", 1);
+	EXPECT_FALSE(a.insert("name1", 1));
+}
+
+TEST(Tables_manager, can_find_element_int_tables)
+{
+	Tables_manager<string, int> a;
+	a.insert("name1", 1);
+	EXPECT_EQ(a.find("name1"), 1);
+}
+
+TEST(Tables_manager, can_delete_element_out_of_tables)
+{
+	Tables_manager<string, int> a;
+	a.insert("name1", 1);
+	EXPECT_TRUE(a.delete_rec("name1"));
+}
+
+TEST(Tables_manager, false_when_try_to_delete_elem_from_empty_tables)
+{
+	Tables_manager<string, int> a;
+	EXPECT_FALSE(a.delete_rec("name1"));
+}
+
+TEST(Tables_manager, false_when_try_to_delete_unexist_elem)
+{
+	Tables_manager<string, int> a;
+	a.insert("name1", 1);
+	EXPECT_FALSE(a.delete_rec("name2"));
+}
+
+TEST(Tables_manager, throws_when_try_to_find_elem_in_empty_tables)
+{
+	Tables_manager<string, int> a;
+	ASSERT_ANY_THROW(a.find("name1"));
+}
+
+TEST(Tables_manager, throws_when_try_to_find_unexisting_elem)
+{
+	Tables_manager<string, int> a;
+	a.insert("name1", 1);
+	ASSERT_ANY_THROW(a.find("name2"));
+}
+
+TEST(Tables_manager, false_when_try_to_insert_elem_in_full_tables)
+{
+	Tables_manager<string, int> a;
+	for (int i = 0; i < max_fill; i++)
+		EXPECT_TRUE(a.insert("name" + to_string(i), i));
+	EXPECT_FALSE(a.insert("name" + to_string(max_fill), max_fill));
+}
+
+TEST(Tables_manager, inserts_into_all_tables_at_once)
+{
+	Tables_manager<string, int> a;
+	a.insert("name", 1);
+	a.set_current(list_table);
+	EXPECT_EQ(a.find("name"), 1);
+}
+
+TEST(Tables_manager, delete_out_of_all_tables_at_once)
+{
+	Tables_manager<string, int> a;
+	a.insert("name", 1);
+	a.delete_rec("name");
+	a.set_current(list_table);
+	EXPECT_FALSE(a.delete_rec("name"));
+}
+
+TEST(Tables_manager, can_find_an_element_in_another_table)
+{
+	Tables_manager<string, int> a;
+	a.insert("name", 1);
+	a.set_current(sorted_array_table);
+	EXPECT_EQ(a.find("name"), 1);
+}
+
+
+
--- LAB1_POLYNOM/tables/tables_manager.h
+++ LAB1_POLYNOM/tables/tables_manager.h
@@ -12,6 +12,7 @@
 
 const size_t count_of_tables = 7;
 
+
 using table_type = size_t;
 
 const table_type array_table = 0;
@@ -25,22 +26,75 @@ const table_type sorted_array_table = 6;
 template<class T, class B>
 class Tables_manager
 {
-	vector<Base_table<T,B>*> tables(count_of_tables);
-	vector<T,B> database;//мб тоже не надо
-	Base_table* current_table;//тут можно тэйбл_тайпом наверное
-	//а может и не лучше
+	struct record
+	{
+		T key;
+		B data;
+	};
+	vector<Base_table<T, B>*> tables;
+	Base_table<T,B>* current_table;
 	size_t tables_fill;
-	bool are_sync;//тут наверное задел на оптимизацию, пока не уверен, нужно ли это (типа синхронизировать таблички только по надобности)
-	void sync();
+	vector<T> deleted;
+	vector<record> inserted;
+	void sync()
+	{
+		for (int i = 0; i < count_of_tables; i++)
+		{
+			for (int j = 0; j < deleted.size(); j++)
+				tables[i]->delete_rec(deleted[j]);
+			for (int k = 0; k < inserted.size(); k++)
+				tables[i]->insert(inserted[k].key, inserted[k].data);
+		}
+		deleted.clear();
+		inserted.clear();
+	}
 public:
-	Tables_manager() :tables_fill{ 0 }, are_sync{ true };
-	~Tables_manager();
-	size_t get_fill();
-	bool get_condition();//возможно во вне это не нужно
-	void set_current(table_type table);
-	T find(const string& name);
-	void insert(const string& name, const T& obj);
-	void pop(const string& name);
+	Tables_manager() :tables_fill{ 0 }
+	{
+		tables.resize(count_of_tables);
+		tables[array_table] = new Array_table<T, B>;
+		tables[avl_tree_table] = new Avl_tree_table<T, B>;
+		tables[hash_chaining_table] = new Hash_chaining_table<T, B>;
+		tables[hash_open_addr_table] = new Hash_open_addr_table<T, B>;
+		tables[list_table] = new List_table<T, B>;
+		tables[rb_tree_table] = new RB_tree<T, B>;
+		tables[sorted_array_table] = new Sorted_array_table<T, B>;
+		current_table = tables[array_table];
+	}
+	void set_current(table_type table)
+	{
+		current_table = tables[table];
+	}
+	const B& find(const T& name)
+	{
+		sync();
+		return current_table->find(name);
+	}
+	bool insert(const T& name, const B& obj)
+	{
+		sync();
+		if (current_table->insert(name, obj))
+		{
+			tables_fill--;
+			inserted.push_back({ name,obj });
+			return true;
+		}
+		else
+			return false;
+	}
+	bool delete_rec(const T& name)
+	{
+		sync();
+		if (current_table->delete_rec(name))
+		{
+			tables_fill++;
+			deleted.push_back(name);
+			return true;
+		}
+		else
+			return false;
+	}
+	~Tables_manager() = default;
 };
 
 #endif
