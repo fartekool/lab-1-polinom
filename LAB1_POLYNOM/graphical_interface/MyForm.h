b077ed83d3d1db268ed9f0483369640513f643ea
fartekool <levonychev2006@gmail.com>
2025-04-02 14:02:13 +0300

    Интеграл и производная

--- LAB1_POLYNOM/graphical_interface/MyForm.h
+++ LAB1_POLYNOM/graphical_interface/MyForm.h
@@ -40,6 +40,9 @@ namespace graphicalinterface {
 			DataGridViewButtonColumn^ derivativeColumn = gcnew DataGridViewButtonColumn();
 			derivativeColumn->HeaderText = "Производная";
 
+			DataGridViewButtonColumn^ integralColumn = gcnew DataGridViewButtonColumn();
+			integralColumn->HeaderText = "Интеграл";
+
 			DataGridViewButtonColumn^ deleteColumn = gcnew DataGridViewButtonColumn();
 			deleteColumn->HeaderText = "Удалить";
 			deleteColumn->Text = "X";
@@ -48,6 +51,7 @@ namespace graphicalinterface {
 			polynomialGridView->Columns->Add("Name", "Имя");
 			polynomialGridView->Columns->Add("Polynom", "Полином");
 			polynomialGridView->Columns->Add(derivativeColumn);
+			polynomialGridView->Columns->Add(integralColumn);
 			polynomialGridView->Columns->Add(deleteColumn);
 		}
 	private:
@@ -62,11 +66,52 @@ namespace graphicalinterface {
 		//		polynomialGridView->Rows[rowIndex]->Cells[1]->Value = gcnew System::String(record.data.GetInfix().c_str());
 		//	}
 		//}
-	private: 
+	private:
 		System::Void polynomialGridView_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e)
 		{	
 		
-			if (e->ColumnIndex == 3) // Нажали на колонку с кнопками "Удалить"
+			
+			if (e->ColumnIndex == 2) // Производная
+			{
+				System::String^ polyName = polynomialGridView->Rows[e->RowIndex]->Cells[0]->Value->ToString();
+				std::string name = msclr::interop::marshal_as<std::string>(polyName);
+				System::Windows::Forms::DialogResult result = MessageBox::Show(
+					"Взять производную по переменной " + "\'" + gcnew String(GetSelectedVariable(), 1)+ "\' " + "у полинома '" + polyName + "' ? ", "Подтверждение",
+					MessageBoxButtons::YesNo, MessageBoxIcon::Warning);
+
+				if (result == System::Windows::Forms::DialogResult::Yes)
+				{
+					Polynom polynom = table.find(name);
+					table.delete_rec(name);
+
+					Polynom new_polynom = polynom.derivative(GetSelectedVariable());
+					table.insert(name, new_polynom);
+
+					polynomialGridView->Rows[e->RowIndex]->Cells[0]->Value = polyName;
+					polynomialGridView->Rows[e->RowIndex]->Cells[1]->Value = gcnew System::String(new_polynom.GetInfix().c_str());
+				}
+			}
+			if (e->ColumnIndex == 3) // Интеграл
+			{
+				System::String^ polyName = polynomialGridView->Rows[e->RowIndex]->Cells[0]->Value->ToString();
+				std::string name = msclr::interop::marshal_as<std::string>(polyName);
+				System::Windows::Forms::DialogResult result = MessageBox::Show(
+					"Взять интеграл по переменной " + "\'" + gcnew String(GetSelectedVariable(), 1) + "\' " + "у полинома '" + polyName + "' ? ", "Подтверждение",
+					MessageBoxButtons::YesNo, MessageBoxIcon::Warning);
+
+				if (result == System::Windows::Forms::DialogResult::Yes)
+				{
+					Polynom polynom = table.find(name);
+					table.delete_rec(name);
+
+					Polynom new_polynom = polynom.integrate(GetSelectedVariable());
+					table.insert(name, new_polynom);
+
+					polynomialGridView->Rows[e->RowIndex]->Cells[0]->Value = polyName;
+					polynomialGridView->Rows[e->RowIndex]->Cells[1]->Value = gcnew System::String(new_polynom.GetInfix().c_str());
+				}
+			}
+			if (e->ColumnIndex == 4) // Удаление
 			{
 				System::String^ polyName = polynomialGridView->Rows[e->RowIndex]->Cells[0]->Value->ToString();
 				std::string name = msclr::interop::marshal_as<std::string>(polyName);
@@ -84,24 +129,15 @@ namespace graphicalinterface {
 
 				}
 			}
-			/*if (e->ColumnIndex == 2) // Производная
-			{
-				System::String^ polyName = polynomialGridView->Rows[e->RowIndex]->Cells[0]->Value->ToString();
-				System::String^ pol = polynomialGridView->Rows[e->RowIndex]->Cells[1]->Value->ToString();
-				std::string name = msclr::interop::marshal_as<std::string>(polyName);
-				Polynom polynom = table.find(name);
-				table.delete_rec(name);
-				arr_table.delete_rec(name);
-
-				Polynom new_polynom = polynom.derivative('x');
-				table.insert(name, new_polynom);
-				arr_table.insert(name, new_polynom);
-
-				polynomialGridView->Rows[e->RowIndex]->Cells[0]->Value = polyName;
-				polynomialGridView->Rows[e->RowIndex]->Cells[1]->Value = gcnew System::String(new_polynom.GetInfix().c_str());
-			}*/
 		}
-
+	private:
+		char GetSelectedVariable()
+		{
+			if (radioButtonX->Checked) return 'x';
+			if (radioButtonY->Checked) return 'y';
+			if (radioButtonZ->Checked) return 'z';
+			return 'x';
+		}
 	protected:
 		~MyForm()
 		{
@@ -116,6 +152,13 @@ namespace graphicalinterface {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::DataGridView^ polynomialGridView;
 	private: System::Windows::Forms::ImageList^ imageList1;
+private: System::Windows::Forms::GroupBox^ groupBox1;
+private: System::Windows::Forms::RadioButton^ radioButtonZ;
+
+private: System::Windows::Forms::RadioButton^ radioButtonY;
+
+private: System::Windows::Forms::RadioButton^ radioButtonX;
+
 	private: System::ComponentModel::IContainer^ components;
 
 
@@ -133,7 +176,12 @@ namespace graphicalinterface {
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->polynomialGridView = (gcnew System::Windows::Forms::DataGridView());
 			this->imageList1 = (gcnew System::Windows::Forms::ImageList(this->components));
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->radioButtonZ = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButtonY = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButtonX = (gcnew System::Windows::Forms::RadioButton());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->polynomialGridView))->BeginInit();
+			this->groupBox1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// textBox1
@@ -174,12 +222,12 @@ namespace graphicalinterface {
 			// 
 			this->polynomialGridView->AllowUserToAddRows = false;
 			this->polynomialGridView->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->polynomialGridView->Location = System::Drawing::Point(528, 81);
+			this->polynomialGridView->Location = System::Drawing::Point(760, 81);
 			this->polynomialGridView->Name = L"polynomialGridView";
 			this->polynomialGridView->ReadOnly = true;
 			this->polynomialGridView->RowHeadersWidth = 82;
 			this->polynomialGridView->RowTemplate->Height = 33;
-			this->polynomialGridView->Size = System::Drawing::Size(1257, 642);
+			this->polynomialGridView->Size = System::Drawing::Size(1214, 642);
 			this->polynomialGridView->TabIndex = 4;
 			this->polynomialGridView->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::polynomialGridView_CellContentClick);
 			// 
@@ -189,12 +237,58 @@ namespace graphicalinterface {
 			this->imageList1->ImageSize = System::Drawing::Size(16, 16);
 			this->imageList1->TransparentColor = System::Drawing::Color::Transparent;
 			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->radioButtonZ);
+			this->groupBox1->Controls->Add(this->radioButtonY);
+			this->groupBox1->Controls->Add(this->radioButtonX);
+			this->groupBox1->Location = System::Drawing::Point(521, 81);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(200, 182);
+			this->groupBox1->TabIndex = 5;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"groupBox1";
+			// 
+			// radioButtonZ
+			// 
+			this->radioButtonZ->AutoSize = true;
+			this->radioButtonZ->Location = System::Drawing::Point(7, 115);
+			this->radioButtonZ->Name = L"radioButtonZ";
+			this->radioButtonZ->Size = System::Drawing::Size(95, 29);
+			this->radioButtonZ->TabIndex = 2;
+			this->radioButtonZ->TabStop = true;
+			this->radioButtonZ->Text = L"По \'z\'";
+			this->radioButtonZ->UseVisualStyleBackColor = true;
+			// 
+			// radioButtonY
+			// 
+			this->radioButtonY->AutoSize = true;
+			this->radioButtonY->Location = System::Drawing::Point(7, 79);
+			this->radioButtonY->Name = L"radioButtonY";
+			this->radioButtonY->Size = System::Drawing::Size(95, 29);
+			this->radioButtonY->TabIndex = 1;
+			this->radioButtonY->TabStop = true;
+			this->radioButtonY->Text = L"По \'y\'";
+			this->radioButtonY->UseVisualStyleBackColor = true;
+			// 
+			// radioButtonX
+			// 
+			this->radioButtonX->AutoSize = true;
+			this->radioButtonX->Location = System::Drawing::Point(7, 43);
+			this->radioButtonX->Name = L"radioButtonX";
+			this->radioButtonX->Size = System::Drawing::Size(95, 29);
+			this->radioButtonX->TabIndex = 0;
+			this->radioButtonX->TabStop = true;
+			this->radioButtonX->Text = L"По \'x\'";
+			this->radioButtonX->UseVisualStyleBackColor = true;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(2047, 1146);
+			this->Controls->Add(this->groupBox1);
 			this->Controls->Add(this->polynomialGridView);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox3);
@@ -203,6 +297,8 @@ namespace graphicalinterface {
 			this->Name = L"MyForm";
 			this->Text = L"Polynom";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->polynomialGridView))->EndInit();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
